7a857cc711742cf39db114353d569e1f8a7fb117
abjklD <abjkl@163.comD>
2017-08-31 22:05:47 +0800

    First submit
    The originin versiont the work

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
 # neighborhood_map
+
+neighborhood_map is the 5th project for Full Stack Web Developer course.
+
+## Browsers & Enviorment & Frameworks
+
+### Browers
+Your browsers should support HTML5. The lastest version of Chrome, Safari and Firefox is fine. If you use IE,  IE10 and above is preferred.
+
+### jQuery
+The work uses ```jQuery 3.2.1 ```.
+
+### Knockoutjs
+The work uses ```knockout.3.4.2 ```. And the file is already in the project.
+
+### font-awesome-4.0.7
+I also use ```font-awesome-4.0.7 ``` to show the icon in the html.
+
+
+## API Used
+
+### Googlemaps JavaScript API
+This api is used for generate the map and handle the popup information of the map.
+
+### Bart informtion API
+The Bart station is get from ```www.bart.gov ```, who supply the coders with api to get the real time information about the stations.
+
+## How to use
+
+### Load the map
+1. **Make sure you have download the files in the  repository **
+2. Open the index.html with your browser.(Chrome, Safari, Firefox or IE10 above.
+
+### Show markers' information
+There are two ways to show marker's information.
+1. Click the markers on the map.
+2. Click the station's name on the left hand list.
+
+### Filter the stations
+You can input whatever you want to search the station you want. The result will show after you click the ** Filter ** button.
+** Please notice ** that the input is  case-sensitive, which means 'Station A' is different from "station a";
+
+### API request error
+If the API form BART load for more than 5 second, there will be a notice on the nav part of the webpage.
+
+## Document
+
+### map_init.js
+This file contains the main code to initiate the map and the list.
+
+### mapstyle.js
+This file contains the value of the styles of  the map. For example, the color of the water and the font-size of the information on the map.
+
+### style.css
+This file define the outlook of the index.html.
+
+### index.html
+You can run the progame by opening this file.
---
+++ js/map_init.js
@@ -0,0 +1,180 @@
+
+/* ======= Get Bart Station's Data From API ======= */
+
+var locations=[];
+var bartUrl = "http://api.bart.gov/api/stn.aspx?cmd=stns&key=ZQZS-58I3-92RT-DWE9&json=y";
+
+$.ajax({
+    timeout : 5000,
+    type:"GET",
+    url: bartUrl,
+    dataType: "json",
+    success:function (data) {
+
+        var st = data.root.stations.station;
+        for (i=0;i<st.length;i++){
+            var l = new Object();
+            l.station = st[i].name;
+            l.address = st[i].address+','+st[i].city;
+            l.location = new Object();
+            l.location.lat=+ st[i].gtfs_latitude;
+            l.location.lng=+ st[i].gtfs_longitude;
+            locations.push(l);
+        }
+        init();
+    },
+    error: function () {
+        // alert ("Cannot load data!")
+        $('#error').text("Can not load BART information")
+    }
+});
+
+
+/* ======= View Model of the List (Use Knockoutjs)======= */
+
+// Create the station list on the left,within the the element with the id of  "station-list"
+
+var listView = {
+    init: function () {
+        ko.applyBindings(new listView.ViewModel());
+        // octopus.search()
+    },
+    ViewModel: function () {
+        var self = this;
+        self.locationData = locations;
+        console.info(self.locationData[1].station);
+        self.id = function (station) {
+			    var stations = [];
+			    for (i=0;i<self.locationData.length;i++){
+			        var s = self.locationData[i].station;
+			        stations.push(s)
+                };
+                var id = stations.indexOf(station);
+                return id;
+
+            };
+
+        self.showInfowWindow = function (data, event) {
+            var s = data.station;
+            var i = self.id(s);
+            populateInfoWindow(markers[i], largeInfowindow)
+        };
+
+        self. listSearch=function(data,event){
+            // set every stations be visible first before start the search.
+            $("li").css("display", "block");
+
+
+            // get input's value
+            var input = $('#station-search').val();
+
+            // filter the <li> element without input's value;
+            var arry = $("li:not(:contains('" + input + "'))");
+
+            // var arry = $("li:not(:contains('" + self.inputSearch + "'))");
+            arry.css("display", "none");
+            var list = [];
+            for (var n = 0; n < arry.length; n++) {
+                var id = arry.eq(n).attr('id');
+                list.push(id);
+            }
+
+            // filter the markers;
+            for (var m = 0; m < list.length; m++) {
+                var i = list[m];
+                markers[i].setMap(null)
+            }
+        };
+
+        self.listWrap = function (data,event) {
+            if ($('.options-box').is(':hidden')){
+            $('.options-box').show();
+            $('#nav').css("left","25%");
+            $('#map').css("left","25%");
+        }
+        else {
+            $('.options-box').hide();
+            $('#nav').css("left","0");
+            $('#map').css("left","0");
+        }
+        }
+    }
+};
+
+
+/* ======= View Model of the Map ======= */
+
+var map;
+var markers = [];
+var largeInfowindow;
+var bounds;
+
+var initMap = function(){
+    largeInfowindow = new google.maps.InfoWindow();
+    bounds = new google.maps.LatLngBounds();
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 37.8332977, lng: -122.3588496},
+        zoom: 12,
+        styles: styles,
+        mapTypeControl: false
+    });
+
+    // Create markers array;
+    for (var i = 0; i < locations.length; i++) {
+        var position = locations[i].location;
+        console.info(position);
+        var title = locations[i].address;
+        var station = locations[i].station;
+        var marker = new google.maps.Marker({
+            position: position,
+            title: title,
+            lable: station,
+            animation: google.maps.Animation.DROP,
+            id: i
+        });
+        markers.push(marker);
+
+        // make every marker can be clicked;
+        marker.addListener('click', function () {
+            populateInfoWindow(this, largeInfowindow);
+
+        });
+
+        // sert markers on the map;
+         markers[i].setMap(map);
+         bounds.extend(markers[i].position)
+    };
+    map.fitBounds(bounds);
+
+};
+
+
+/* ======= Handle popup of the Information Window ======= */
+
+var populateInfoWindow = function (marker, infowindow) {
+    if (marker.getAnimation() !== null) {
+                marker.setAnimation(null);
+    }
+    else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function() {
+            marker.setAnimation(null);
+            }, 1480);
+    }
+    if (infowindow.marker != marker) {
+            infowindow.marker = marker;
+            infowindow.setContent('<div><h2>' + marker.lable + '</h2><p>' + marker.title + '</p></div>');
+            infowindow.open(map, marker);
+            infowindow.addListener('closeclick', function () {
+                infowindow.marker = null;
+            });
+        }
+};
+
+/* ======= Start! ======= */
+
+var init = function() {
+    initMap();
+    listView.init();
+
+};
